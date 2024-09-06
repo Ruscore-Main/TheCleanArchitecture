02c89e010392da17bd33c150d7c2429cc167b1bb
Ruscore-Main <oobitovich@mail.ru>
2024-09-06 20:04:08 +0300

    init commit

---
+++ Notes.Domain/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notes.Domain
+{
+    public class Note
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime EditDate { get; set; }
+    }
+}
---
+++ Notes.Persistence/DbInitializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notes.Persistence
+{
+    public class DbInitializer
+    {
+        public static void Initialize(NotesDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
